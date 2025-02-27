99c137ff449f52994d184bbca99749ed28a55d40
Shoaib Shaikh <shoaibsshaikh1999@gmail.com>
2025-02-27 11:03:08 +0530

    Add data and provider

--- clouds/aws/dev/provider.tf
+++ clouds/aws/dev/provider.tf
@@ -15,15 +15,24 @@ terraform {
   required_version = "~> 1.10.5"
 }
 
-# provider "kubernetes" {
-#   host                   = module.eks.cluster_endpoint
-#   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-#   exec {
-#     api_version = "client.authentication.k8s.io/v1beta1"
-#     args        = ["eks", "get-token", "--cluster-name", "${var.project_name}-eks"]
-#     command     = "aws"
-#   }
-# }
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  token = data.aws_eks_cluster_auth.cluster.token
+  # exec {
+  #   api_version = "client.authentication.k8s.io/v1beta1"
+  #   args        = ["eks", "get-token", "--cluster-name", "${var.project_name}-eks"]
+  #   command     = "aws"
+  # }
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = data.aws_eks_cluster.cluster.endpoint
+    token                  = data.aws_eks_cluster_auth.cluster.token
+    cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  }
+}
 
 provider "aws" {
   # Configuration options
